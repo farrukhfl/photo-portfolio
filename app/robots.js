5293e11bda869c7f09f5b4475535c93937425699
Farrukh Shahzad <furrukhshahzad172@gmail.com>
2026-07-20 17:18:26 +0500

    node version change

--- app/robots.js
+++ app/robots.js
@@ -7,6 +7,6 @@ export default function robots() {
       allow: '/',
       disallow: ['/admin', '/api'],
     },
-    sitemap: `${SITE.url.replace(/\/$/, '')}/sitemap.xml`,
+    sitemap: 'https://farrukh-photography.vercel.app/sitemap.xml',
   };
 }
